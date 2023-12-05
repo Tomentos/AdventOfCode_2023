58453340e33f9b9409717312126ebee48fb56222
Tomentos <thomas.gutknecht01@gmail.com>
2023-12-06 00:30:36 +0100

    Completed challenge for day 5

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ To execute the code, follow these steps:
 4. Run `npm install` and let all the dependencies install themselves.
 5. Run `npm run main` to start the program. Follow the documentation below to figure out how to use it.
 
-This years advent of code has two puzzles for each day. The current amount of days in the repository is `4`. Every day has both challenges completed.
+This years advent of code has two puzzles for each day. The current amount of days in the repository is `5`. Every day has both challenges completed.
 To run a specific challenge, first enter the day for which you want to run a challenge for. After that, enter either `1` or `2` to run the script for the specific challenge. The result will be printed as a console log.
 
 **The input is stored in a text file which is saved in each days folder. If you need to replace the input, you need to do that manually.**
+
+***DAY 5 PART 2 IS UNTESTED WITH THE ACTUAL INPUT, ONLY SAMPLE DATA WAS TESTED AND SUCCEEDED***
---
+++ day5/chall2.js
@@ -0,0 +1,117 @@
+//Import required libraries
+const fs = require('fs');
+
+//Read input data
+fs.readFile('./day5/input.txt', 'utf8', (err, data) => {
+
+    //Error event for readFile function
+    if (err) {
+        console.log('Error when reading input.txt File.\nMake sure the file exists and contains the correct contents.\n' + err);
+        return;
+    }
+
+    //Seperate the input into lines
+    data = data.split('\n');
+
+    //Create base variables
+    var navigation = ['seeds', 'soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity', 'location'];
+    var indexScroll = 0;
+    var result = [];
+    result['seeds'] = [];
+    result['soil'] = [];
+    result['fertilizer'] = [];
+    result['water'] = [];
+    result['light'] = [];
+    result['temperature'] = [];
+    result['humidity'] = [];
+    result['location'] = [];
+    
+    //Save the seeds in a seperate variable
+    result['seeds'] = data[0].split(' ');
+    result['seeds'] = result['seeds'].splice(1, result['seeds'].length - 1);
+    result['seeds'] = result['seeds'].map(function (x) { return parseInt(x); });
+    data = data.splice(2, data.length - 1);
+    
+    //For some reason the seeds didn't let themselves be saved directly into the seedsTemp variable so I copied the result['seeds'] variables into it and then redeclared it, I don't care anymore at this point I'm fucking fed up with this shit let me go to bed for fucks sake
+    var seedsTemp = result['seeds'];
+    result['seeds'] = [];
+
+    //Turn the seed amounts into ranges
+    for (let i = 0; i < seedsTemp.length; i++) {
+        var start = seedsTemp[i];
+        var range = seedsTemp[i + 1];
+
+        //Loop through the numbers as many times as the range allows
+        for (let j = 0; j < range; j++) {
+
+            //Push the seed into the array and shed a tear with each and every new seed placed in there as the solution for part 1 already took 15 minutes to complete and this will have many many many more seeds now because of this shit im boutta cry man i cannot go on like this anymore
+            result['seeds'].push(start + j);
+        }
+
+        i++;
+    }
+
+    //Loop through every line of the input
+    for (line of data) {
+
+        //If the line contains a title / is the beginning of a new data set
+        if (line.includes('-to-')) {
+
+            //Increase the indexScroll and continue to the next loop
+            indexScroll++;
+            continue;
+        }
+
+        //If the line is empty
+        else if (line === '') {
+
+            //Continue to the next loop
+            continue;
+        }
+
+        //If the line contains data to be calculated
+        else {
+
+            //Split the values and enter them into more managable variables
+            line = line.split(' ');
+            var start = +line[1];
+            var goal = +line[0];
+            var range = +line[2];
+
+            //Loop through the numbers as many times as the range allows
+            for (let i = 0; i < range; i++) {
+
+                //Get all final numbers of the previous data set
+                var prev = result[navigation[indexScroll - 1]]
+
+                //Check if the currently looped number is contained in the previous data set
+                var index = prev.indexOf(+start + i);
+
+                //If the number was found
+                if (index != -1) {
+
+                    //Save the destination number in the same position of the current data set
+                    result[navigation[indexScroll]][index] = goal + i;
+                }
+            }
+        }
+
+        //Loop through all numbers saved in the current data set
+        for (let i = 0; i < result[navigation[indexScroll - 1]].length; i++) {
+
+            //If one number was not defined in the loop
+            if (result[navigation[indexScroll]][i] === undefined) {
+
+                //Copy the number from the same position of the previous data set
+                result[navigation[indexScroll]][i] = result[navigation[indexScroll - 1]][i];
+            }
+        }
+    }
+
+    //Find the lowest location number
+    const location = Math.min(...result['location']);
+
+    //Print the full table and the final result
+    console.table(result);
+    console.log(`The location which can go fuck itself is: ${location}`)
+});
