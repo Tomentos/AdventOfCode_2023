3803158daa8d95af5db6765dd3458b57bf1e1374
Tomentos <thomas.gutknecht01@gmail.com>
2023-12-06 14:13:41 +0100

    Wrote more memory efficient brute force for day 5 challenge 2

--- day5/chall2.js
+++ day5/chall2.js
@@ -15,29 +15,33 @@ fs.readFile('./day5/input.txt', 'utf8', (err, data) => {
 
     //Create base variables
     var navigation = ['seeds', 'soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity', 'location'];
-    var indexScroll = 0;
     var result = [];
-    result['seeds'] = [];
-    result['soil'] = [];
-    result['fertilizer'] = [];
-    result['water'] = [];
-    result['light'] = [];
-    result['temperature'] = [];
-    result['humidity'] = [];
-    result['location'] = [];
+    var seedsTemp = [];
+    var location = 0;
     
     //Save the seeds in a seperate variable
-    result['seeds'] = data[0].split(' ');
-    result['seeds'] = result['seeds'].splice(1, result['seeds'].length - 1);
-    result['seeds'] = result['seeds'].map(function (x) { return parseInt(x); });
+    seedsTemp = data[0].split(' ');
+    seedsTemp = seedsTemp.splice(1, seedsTemp.length - 1);
+    seedsTemp = seedsTemp.map(function (x){ return parseInt(x); });
     data = data.splice(2, data.length - 1);
     
-    //For some reason the seeds didn't let themselves be saved directly into the seedsTemp variable so I copied the result['seeds'] variables into it and then redeclared it, I don't care anymore at this point I'm fucking fed up with this shit let me go to bed for fucks sake
-    var seedsTemp = result['seeds'];
-    result['seeds'] = [];
-
     //Turn the seed amounts into ranges
-    for (let i = 0; i < seedsTemp.length; i++) {
+    for (let i = 0; i < seedsTemp.length; i = i + 2) {
+        
+        //Initialize the data set
+        result['seeds'] = [];
+        result['soil'] = [];
+        result['fertilizer'] = [];
+        result['water'] = [];
+        result['light'] = [];
+        result['temperature'] = [];
+        result['humidity'] = [];
+        result['location'] = [];
+
+        //Start the counter for scrolling through the table index
+        var indexScroll = 0;
+        
+        //Define start start seed and the seed range
         var start = seedsTemp[i];
         var range = seedsTemp[i + 1];
 
@@ -47,71 +51,75 @@ fs.readFile('./day5/input.txt', 'utf8', (err, data) => {
             //Push the seed into the array and shed a tear with each and every new seed placed in there as the solution for part 1 already took 15 minutes to complete and this will have many many many more seeds now because of this shit im boutta cry man i cannot go on like this anymore
             result['seeds'].push(start + j);
         }
-
-        i++;
-    }
-
-    //Loop through every line of the input
-    for (line of data) {
-
-        //If the line contains a title / is the beginning of a new data set
-        if (line.includes('-to-')) {
-
-            //Increase the indexScroll and continue to the next loop
-            indexScroll++;
-            continue;
-        }
-
-        //If the line is empty
-        else if (line === '') {
-
-            //Continue to the next loop
-            continue;
-        }
-
-        //If the line contains data to be calculated
-        else {
-
+        
+        //Loop through every line of the input
+        for (line of data) {
+    
+            //If the line contains a title / is the beginning of a new data set
+            if (line.includes('-to-')) {
+    
+                //Increase the indexScroll and continue to the next loop
+                indexScroll++;
+                continue;
+            }
+    
+            //If the line is empty
+            else if (line === '') {
+    
+                //Continue to the next loop
+                continue;
+            }
+    
             //Split the values and enter them into more managable variables
             line = line.split(' ');
             var start = +line[1];
             var goal = +line[0];
             var range = +line[2];
-
-            //Loop through the numbers as many times as the range allows
-            for (let i = 0; i < range; i++) {
-
-                //Get all final numbers of the previous data set
-                var prev = result[navigation[indexScroll - 1]]
-
-                //Check if the currently looped number is contained in the previous data set
-                var index = prev.indexOf(+start + i);
-
-                //If the number was found
-                if (index != -1) {
-
-                    //Save the destination number in the same position of the current data set
-                    result[navigation[indexScroll]][index] = goal + i;
+        
+                //Loop through the numbers as many times as the range allows
+                for (let j = 0; j < range; j++) {
+    
+                    //Get all final numbers of the previous data set
+                    var prev = result[navigation[indexScroll - 1]];
+    
+                    //Check if the currently looped number is contained in the previous data set
+                    var index = prev.indexOf(+start + j);
+    
+                    //If the number was found
+                    if (index != -1) {
+                    
+                        //Save the destination number in the same position of the current data set
+                        result[navigation[indexScroll]][index] = goal + j;
+                    }
+               
+                    //Loop through all numbers saved in the current data set
+                    for (let j = 0; j < result[navigation[indexScroll - 1]].length; j++) {
+    
+                        //If one number was not defined in the loop
+                        if (result[navigation[indexScroll]][j] === undefined) {
+    
+                            //Copy the number from the same position of the previous data set
+                            result[navigation[indexScroll]][j] = result[navigation[indexScroll - 1]][j];
+                        }
+                    }
                 }
-            }
         }
 
-        //Loop through all numbers saved in the current data set
-        for (let i = 0; i < result[navigation[indexScroll - 1]].length; i++) {
-
-            //If one number was not defined in the loop
-            if (result[navigation[indexScroll]][i] === undefined) {
-
-                //Copy the number from the same position of the previous data set
-                result[navigation[indexScroll]][i] = result[navigation[indexScroll - 1]][i];
+        //Find the lowest location number
+        if (location === 0) {
+            location = Math.min(...result['location']);
+        }
+        else {
+            lowLoc = Math.min(...result['location']);
+            if (location > lowLoc) {
+                location = lowLoc;
             }
         }
+    
+        //Print the table
+        console.table(result);
     }
 
-    //Find the lowest location number
-    const location = Math.min(...result['location']);
 
-    //Print the full table and the final result
-    console.table(result);
     console.log(`The location which can go fuck itself is: ${location}`)
 });
